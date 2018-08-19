06c7ba28592bf3715a21e25d46d9a8f5ba62b38e
linsi <3027311375@qq.com>
2018-08-19 17:21:35 +0800

    修改包名后，文件结构一起修改

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -1,13 +1,13 @@
-package android.coolweather.com.coolweather;
+package com.coolweather.android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(com.coolweather.android.R.layout.activity_main);
     }
 }
